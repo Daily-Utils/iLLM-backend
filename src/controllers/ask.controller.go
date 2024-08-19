58737357430fd4b101065e48787a7e98e5b3b23b
siddh34 <ssiddharthsutar@gmail.com>
2024-08-19 15:36:43 +0530

    removed routes folder

--- src/controllers/ask.controller.go
+++ src/controllers/ask.controller.go
@@ -1,2 +1,24 @@
 package controllers
 
+import (
+	"io/ioutil"
+	"net/http"
+
+	// "github.com/daily-utils/iLLM-backend/src/models"
+	"github.com/gin-gonic/gin"
+)
+
+func Ask(c *gin.Context){
+	// extract variables from response body
+	bodyBytes, err := ioutil.ReadAll(c.Request.Body)
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	prompt := string(bodyBytes)
+
+	// generate response
+	c.JSON(http.StatusOK, gin.H{"prompt": prompt})
+}
