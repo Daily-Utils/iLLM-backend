6a885052d3907a2f5202462c4d47ceb0e711c251
siddh34 <ssiddharthsutar@gmail.com>
2024-08-24 16:56:31 +0530

    added pdf parser

--- src/utils/parse_pdf.go
+++ src/utils/parse_pdf.go
@@ -1,31 +1,58 @@
 package utils
 
-// import (
-// 	"bytes"
-// 	"fmt"
-//     "github.com/pdfcpu/pdfcpu/pkg/api"
-//     "github.com/pdfcpu/pdfcpu/pkg/pdfcpu"
-// 	"os"
-// )
+import (
+	"encoding/json"
+	"fmt"
+	"os"
 
-// func ParsePDFToString(filePath string) (string, error) {
-//     // Read the PDF file
-//     fileBytes, err := os.ReadFile(filePath)
-//     if err != nil {
-//         return "", fmt.Errorf("failed to read file: %v", err)
-//     }
+	"github.com/unidoc/unipdf/v3/extractor"
+	"github.com/unidoc/unipdf/v3/model"
+)
 
-//     // Create a PDF context
-//     ctx, err := api.ReadContext(bytes.NewReader(fileBytes), pdfcpu.DefaultImportConfig())
-//     if err != nil {
-//         return "", fmt.Errorf("failed to create PDF context: %v", err)
-//     }
+func ParsePDFToString(filePath string) (string, error) {
+	// Read the PDF file
+    file, err := os.Open(filePath)
+    if err != nil {
+		return "", fmt.Errorf("failed to read file: %v", err)
+    }
+	defer file.Close()
 
-//     // Extract content from the PDF
-//     var buf bytes.Buffer
-//     if err := api.ExtractContent(ctx, &buf); err != nil {
-//         return "", fmt.Errorf("failed to extract content: %v", err)
-//     }
+	pdfReader, err := model.NewPdfReader(file)
+	if err != nil {
+		return "", fmt.Errorf("failed to create pdf reader: %v", err)
+	}
 
-//     return buf.String(), nil
-// }
+	numPages, err := pdfReader.GetNumPages()
+
+	if err != nil {
+		return "", fmt.Errorf("failed to get number of pages: %v", err)
+	}
+
+	text := ""
+	textArr := []string{}
+	for i := 0; i < min(numPages, 60); i++ {
+		pageNum := i + 1
+		page, err := pdfReader.GetPage(pageNum)
+		if err != nil {
+			return "", fmt.Errorf("failed to get page: %v", err)
+		}
+		ex, err := extractor.New(page)
+		if err != nil {
+			return "", fmt.Errorf("failed to create text extractor: %v", err)
+		}
+		pageText, err := ex.ExtractText()
+		if err != nil {
+			return "", fmt.Errorf("failed to extract text: %v", err)
+		}
+		text += pageText
+		textArr = append(textArr, pageText)
+		text = ""
+	}
+
+	jsonText, err := json.Marshal(textArr)
+	if err != nil {
+		return "", fmt.Errorf("failed to marshal text: %v", err)
+	}
+	
+	return string(jsonText), nil
+}
