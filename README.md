28a2c20210eac54b6254d60e897f460fb4d521a8
siddh34 <ssiddharthsutar@gmail.com>
2024-08-24 18:01:10 +0530

    added code for prompt

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Entire iLLM backend
 
+## Impliment swagger using
+
+https://github.com/swaggo/gin-swagger
--- src/controllers/ask.controller.go
+++ src/controllers/ask.controller.go
@@ -1,11 +1,13 @@
 package controllers
 
 import (
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"time"
 
-	// "github.com/daily-utils/iLLM-backend/src/models"
 	"github.com/daily-utils/iLLM-backend/src/models"
 	"github.com/gin-gonic/gin"
 )
@@ -36,6 +38,46 @@ func Ask(c *gin.Context) {
 		Stream: false,
 	}
 
+	promptJSON, err := json.Marshal(prompt)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	client := &http.Client{
+        Timeout: time.Minute * 5,
+    }
+
+	LLAMA_API := os.Getenv("LLAMA_API")
+
+	// hit the api
+	req, err := http.NewRequest("POST", LLAMA_API + "api/generate", bytes.NewBuffer(promptJSON))
+	if(err != nil){
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate response"})
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
 	// Use the prompt variable as needed
-	c.JSON(http.StatusOK, gin.H{"prompt": prompt})
+	c.JSON(http.StatusOK, gin.H{"response": string(body)})
 }
