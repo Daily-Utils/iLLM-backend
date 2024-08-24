e4e81992ead892e4a9a7645a01690492a2b03bbe
siddh34 <ssiddharthsutar@gmail.com>
2024-08-24 16:31:22 +0530

    corrected ollama

--- ollamaEntryPoint.sh
+++ ollamaEntryPoint.sh
@@ -1,30 +1,11 @@
 #!/bin/sh
 
-curl -sSL https://ollama.com/install.sh | sh
+ollama serve &
 
-# Start the ollama server in the background and redirect output to a log file
-# ollama serve > /app/ollama_serve.log 2>&1 &
-ollama serve
+echo "Waiting for Ollama server to be active..."
+while [ "$(ollama list | grep 'NAME')" == "" ]; do
+    sleep 1
+done
 
 
-# Wait for the server to start
-# sleep 20
-
-# run_model() {
-#   local retries=5
-#   local count=0
-#   echo "Im hereeee"
-#   until ollama run llama3 || [ $count -eq $retries ]; do
-#     count=$((count + 1))
-#     echo "Retrying to run the model... Attempt $count/$retries"
-#     sleep 10
-#   done
-
-#   if [ $count -eq $retries ]; then
-#     echo "Failed to run the model after $retries attempts."
-#     exit 1
-#   fi
-# }
-
-# Run the model with retries in a child process
-# run_model
+# ollama pull llama3-gradient
