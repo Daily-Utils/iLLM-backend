00dee61028507d0c6a49812b4b85fe06a6b5eea7
siddh34 <ssiddharthsutar@gmail.com>
2024-10-14 11:13:41 +0530

    feat: csv context

--- src/controllers/context.csv.controller.go
+++ src/controllers/context.csv.controller.go
@@ -1,10 +1,53 @@
 package controllers
 
 import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/daily-utils/iLLM-backend/src/models"
+	"github.com/daily-utils/iLLM-backend/src/utils"
 	"github.com/gin-gonic/gin"
-	
 )
 
 func ProvideContextForCSV(c *gin.Context) {
-	
+	bodyBytes, err := io.ReadAll(c.Request.Body)
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	var requestBody models.RequestBodyForCSV
+	if err := json.Unmarshal(bodyBytes, &requestBody);
+
+	err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
+	}
+
+	prompt := "I, personally, think that the following text is very interesting. Please keep it in your context will ask you questions on this context: " + requestBody.Body
+
+	promptModel := models.Ask{
+		Model:  requestBody.Model,
+		Prompt: prompt,
+		Stream: false,
+	}
+
+	bodyContent, err := utils.RequestClient(promptModel)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	body := bodyContent
+
+	var response models.Response
+
+	if err := json.Unmarshal([]byte(body), &response); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"response": response})
 }
