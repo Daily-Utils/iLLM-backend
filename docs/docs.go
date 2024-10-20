92fb86af35fe0341cf7b1358ef62e95efb398594
siddh34 <ssiddharthsutar@gmail.com>
2024-10-20 20:55:34 +0530

    feat: done with txt

--- docs/docs.go
+++ docs/docs.go
@@ -259,7 +259,8 @@ const docTemplate = `{
             "post": {
                 "description": "Provide context for the model for text",
                 "consumes": [
-                    "application/json"
+                    "application/json",
+                    "multipart/form-data"
                 ],
                 "produces": [
                     "application/json"
@@ -270,13 +271,16 @@ const docTemplate = `{
                 "summary": "Provide context for text",
                 "parameters": [
                     {
-                        "description": "Request body",
-                        "name": "body",
-                        "in": "body",
-                        "required": true,
-                        "schema": {
-                            "$ref": "#/definitions/controllers.ContextRequestBodyForText"
-                        }
+                        "type": "string",
+                        "name": "model",
+                        "in": "formData"
+                    },
+                    {
+                        "type": "file",
+                        "description": "File to upload",
+                        "name": "file",
+                        "in": "formData",
+                        "required": true
                     }
                 ],
                 "responses": {
@@ -349,17 +353,6 @@ const docTemplate = `{
                     "type": "string"
                 },
                 "text": {
-                    "type": "string"
-                }
-            }
-        },
-        "controllers.ContextRequestBodyForText": {
-            "type": "object",
-            "properties": {
-                "contextProvider": {
-                    "type": "string"
-                },
-                "model": {
                     "type": "string"
                 }
             }
